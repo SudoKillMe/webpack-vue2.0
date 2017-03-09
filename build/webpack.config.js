21634930c003875fed9608e9a3a412bc3d93fdfb
techer <langcundong@gmail.com>
2017-03-09 23:05:26 +0800

    first

---
+++ build/webpack.config.js
@@ -0,0 +1,49 @@
+var path = require( 'path' );
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
+module.exports = {
+
+    entry: path.resolve( __dirname, '../app/index/index.js' ),
+
+    output: {
+        path: path.resolve( __dirname, '../output/static' ),
+        publicPath: 'static/',
+        filename: '[name].[hash].js',
+        chunkFilename: '[id][chunkhash].js'
+    },
+    resolve: {
+        extensions: [ '', '.js', '.vue' ],
+        alias: {
+            'Vue': 'vue/dist/vue.js'
+        }
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel?presets=es2015',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    Favlist: {
+        loaders: {
+            js: 'babel'
+        }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin( {
+            filename: '../index.html',
+            template: path.resolve( __dirname, '../app/index/index.html' ),
+            inject: true
+        } )
+    ]
+
+}
